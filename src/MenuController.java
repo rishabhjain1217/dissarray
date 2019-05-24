18eadd84752fa1fbb41098ca191424ea2cd1f051
Rishabh Jain <rishabhjain1217@gmail.com>
2019-05-24 13:49:01 -0400

    Starter

--- src/MenuController.java
+++ src/MenuController.java
@@ -1,9 +1,14 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -41,9 +46,25 @@ public class MenuController implements Initializable {
     }
 
     public void startButton(){
-        startButton.setOnAction(e -> {
-                ArrayApplication aa = new ArrayApplication();
-                //aa.start();
+
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                try {
+                    BorderPane gamePane = fxmlLoader.load(getClass().getResource("GamePane.fxml").openStream());
+                    GameController gameController = (GameController) fxmlLoader.getController();
+                    gameController.setGameMode(GameController.GameMode.Both);
+                    gameController.start();
+
+                    Scene scene = new Scene(gamePane, 600, 400);
+                    pStage.setTitle("Array Game");
+
+                    pStage.setScene(scene);
+                    pStage.show();
+                } catch (Exception ex) {
+                    System.out.println("Wassup");
+                }
+            }
         });
 
     }
