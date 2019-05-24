5e95066d0cfe795af0bb89f362b967c6d5bc8b8e
1034076 <eamon.mahoney1717@gmail.com>
2019-05-24 10:22:12 -0400

    check question method commented out

--- src/MenuController.java
+++ src/MenuController.java
@@ -1,5 +1,16 @@
+import javafx.application.Application;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Rishabh Jain AKA CodeGod on 05 24, 2019 at 09:26
  */
-public class MenuController {
+public class MenuController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) { }//no code
+
+
+
+
 }
