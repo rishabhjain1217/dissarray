1729888b7ec90caec1936f7d41608efc13c62b25
Rishabh Jain <rishabhjain1217@gmail.com>
2019-06-05 07:33:31 -0400

    mp3 shit

--- src/BackgroundMusic.java
+++ src/BackgroundMusic.java
@@ -1,6 +1,14 @@
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+import javazoom.jl.player.advanced.AdvancedPlayer;
+import javazoom.jl.player.advanced.PlaybackEvent;
+import javazoom.jl.player.advanced.PlaybackListener;
+
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import java.io.File;
+import java.io.*;
 
 public class BackgroundMusic {
     private static BackgroundMusic INSTANCE = new BackgroundMusic();
@@ -10,6 +18,10 @@ public class BackgroundMusic {
     private static long clipTime = 0;
     private final String nameOfMusic = "src/resources/sounds/Wii Remix.wav"; //Must add src to the beginning to specify path
     private final String nameOfMemeMusic = "src/resources/sounds/Wii Remix BOOSTED.wav";
+    private Player player;
+    private FileInputStream fis;
+
+    private static long location = 0;
 
     private boolean alternateSounds = false;
 
@@ -25,10 +37,10 @@ public class BackgroundMusic {
         return isRunning;
     }
 
-    public void play() {
+   /* public void play() {
         if(!isRunning) {
             try {
-                File file = new File(this.alternateSounds ? nameOfMemeMusic : nameOfMusic);
+                File file = new File(nameOfMusic);//this.alternateSounds ? nameOfMemeMusic : nameOfMusic);
                 this.clip = AudioSystem.getClip();
                 clip.open(AudioSystem.getAudioInputStream(file));
                 clip.setMicrosecondPosition(clipTime);
@@ -42,19 +54,122 @@ public class BackgroundMusic {
         else{
             System.out.println("Hello");
         }
-    }//hi
+    }
+
+   public void play() {
+        if(!isRunning) {
+            try {
+                FileInputStream fis = new FileInputStream(nameOfMusic);
+                //fis.skip(location);
+                BufferedInputStream bis = new BufferedInputStream(fis);
+                //bis.skip(location);
+                player = new AdvancedPlayer(bis);
+                player.setPlayBackListener(new PlaybackListener() {
+                    @Override
+                    public void playbackFinished(PlaybackEvent event) {
+                        System.err.println(event.getFrame());
+                        location = event.getFrame();
+                    }
+
+                });
+            } catch (Exception e) {
+                System.out.println("Problem playing file ");
+                System.out.println(e);
+            }
+
+            // run in new thread to play in background
+            new Thread() {
+                public void run() {
+                    try {
+                        player.play(location, Integer.MAX_VALUE);
+                        //player.play();
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                }
+            }.start();
+        }
+        else{
+            System.out.println("Hello");
+        }
+    }
+    */
+
+
+    /*public void mute() {
+        if(!mute){
+            isRunning = false;
+            mute = true;
+            System.out.println("yo");
+            //clipTime = clip.getMicrosecondPosition();
+           // clip.stop();
+            //player.close();
+            thingy.pause();
+        }
+        else{
+            mute = false;
+            //play();
+            try {
+                thingy.play();
+            } catch (JavaLayerException e) {
+                e.printStackTrace();
+            }
+        }
+    }*/
+
+    public void play(){
+        if(!isRunning) {
+            System.out.println("wassup");
+            try {
+                new Thread() {
+                    public void run() {
+                        try {
+                            player.play();
+                        } catch (Exception e) {
+                            System.out.println(e);
+                        }
+                    }
+                }.start();
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        else{
+            System.out.println("Hello");
+        }
+
+    }
 
     public void mute(){
         if(!mute){
             isRunning = false;
             mute = true;
-            clipTime = clip.getMicrosecondPosition();
-            clip.stop();
+            System.out.println("yo");
+            try {
+                location = fis.read();
+                System.out.println(location);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            player.close();
         }
         else{
+            try {
+                File f = new File("src/resources/sounds/idkman.mp3");
+                System.out.println(f.length());
+                fis = new FileInputStream(f);
+                fis.skip((f.length() - location));
+                player = new Player(fis);
+                //fis.skip(location);
+            } catch (JavaLayerException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             mute = false;
             play();
         }
+
     }
 
     public void useAlternate()
