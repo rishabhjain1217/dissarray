b0d7d6e1aabbba23bc26ce4b1ef1dfafc9f2655e
Rishabh Jain <rishabhjain1217@gmail.com>
2019-06-06 09:04:13 -0400

    TwoDim Constants

--- src/Constants.java
+++ src/Constants.java
@@ -12,7 +12,49 @@ public interface Constants {
 
 
 
-//jah
-//h
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //TwoDimQuestion
+    int TIME_FOR_ELEMENT_QUESTION_TWODIM = 15; //Element Question Time TwoDim
+    int TIME_FOR_RANGE_QUESTION_TWODIM = 25; //Range Question Time TwoDim
+    int MAX_RANGED_DETRACTION = 13; //Max amount of time taken away from questions
+    int MAX_ROWS_TWODIM = 3;
+    int MAX_COLS_TWODIM = 6;
+    int MIN_ROWS_TWODIM = 3;
+    int MIN_COLS_TWODIM = 3;
+    int SWITCH_CASE_SCORE_TWODIM = 10;
 
 }
